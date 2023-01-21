12f7599c836fb22b44b59bf8733e3ba7fae6ea3b
Thet Oo Aung <thetooaung07@gmail.com>
2023-01-21 23:20:05 +0630

    Clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import "./App.css";
-import { NavBar } from "./components/NavBar";
-import { CtaSection } from "./Sections/CtaSection";
-import { FeatureSection } from "./Sections/FeatureSection";
-import { Footer } from "./Sections/Footer";
-import { HeroSection } from "./Sections/HeroSection";
-import { TestimonialSection } from "./Sections/TestimonialSection";
 
 function App() {
-  return (
-    <div>
-      <NavBar></NavBar>
-      <HeroSection></HeroSection>
-      <FeatureSection></FeatureSection>
-      <TestimonialSection></TestimonialSection>
-      <CtaSection></CtaSection>
-      <Footer></Footer>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
